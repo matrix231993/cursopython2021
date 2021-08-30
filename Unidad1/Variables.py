001be2987340b649efc532ad33dad21997a0e052
GEOVANNY <josechavez23@outlook.com>
2021-08-29 22:53:05 -0500

    first commit

---
+++ Unidad1/Variables.py
@@ -0,0 +1,47 @@
+
+#Datos de tipo Numerico
+
+#PEP8
+numero_1 = 1.5 
+print (id(numero_1))
+print (type(numero_1))
+
+numero_2 = 10
+print (id(numero_2))
+print (type(numero_2))
+
+#PEP8 : Estandares de Codificacion  Comunidad de Oython
+
+# Definir Variable de tipo STRING
+
+cadena_1 = "Hola"
+cadena_2 = "Mundo"
+cadena_3 = "Desde Python"
+
+print("La variable cadenal : ",type(cadena_1))
+print("La variable cadena2 : ",type(cadena_2))
+print("La variable cadena3 : ",type(cadena_3))
+
+cadena_3 = 10.5
+print("La variable cadena333 : ",type(cadena_3))
+
+cadena_3 = 100
+print("La variable cadena444 : ",type(cadena_3))
+
+# Tipos de Literales que tenemos python : Datos Primitivos
+
+variable_1 = ""  # Variables de String
+variable_2 = ''  # Variables de String
+variable_3 = 10  # Variable de tipo Entero
+variable_4 = 10.5 # Variable de tipo Float
+variable_5 = "true" # variable de tipo bool
+variable_5 = "false" # variable de tipo bool
+
+#Funciones predefinidas de Python
+print() # Sirve para enviar a consola los mensajes
+id()   #Me devuelve la posicion de memoria en donde esta almacena la variable
+type() # Tipo de dato q contiene la variable
+
+# Para comentar el codigo tenemos
+# Comentarios de 1 sola linea con el signo "#"
+# Comentarios de 1 sola lineas con el signo """sumary_line"""
